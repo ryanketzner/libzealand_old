d15d19a65f70d336f2f963de1db3f7ad3060942f
Ryan Ketzner <rketzner@knights.ucf.edu>
2023-02-19 16:02:39 -0800

    Modified contains function based on profiling for SphereView. 50% -> 40%.

--- SphereView.hpp
+++ SphereView.hpp
@@ -20,12 +20,50 @@ public:
         return query(box,sphere).intersect;
     }
 
+    // bool contains(const AlignedBox3& box) override
+    // {
+    //     std::array<Vector3,8> vertices;
+    //     box.GetVertices(vertices);
+
+    //     for (int i = 0; i < vertices.size(); i++)
+    //     {
+    //         // If any vertex is not in the container,
+    //         // immediately terminate
+    //         if (!gte::InContainer(vertices[i], sphere))
+    //             return false;
+    //     }
+    //     // All vertices are in container
+    //     return true;
+    // }
+
     bool contains(const AlignedBox3& box) override
     {
+        if (!gte::InContainer(box.min, sphere))
+            return false;
+        else if (!gte::InContainer(box.max, sphere))
+            return false;
+
+        // Vector3 vertex;
+
+        // // min min max
+        // vertex[0] = box.min[0];
+        // vertex[1] = box.min[1];
+        // vertex[2] = box.max[1];
+
+        // if (!gte::InContainer(box.min, sphere)
+        //     return false;
+
+        // // min max max
+        // vertex[1] = box.max[1];
+
+        // if (!gte::InContainer(box.min, sphere)
+        //     return false;
+
+        // // min max min
+        // vertex[2] = box.min[1];
         std::array<Vector3,8> vertices;
         box.GetVertices(vertices);
-
-        for (int i = 0; i < vertices.size(); i++)
+        for (int i = 1; i < vertices.size() - 1; i++)
         {
             // If any vertex is not in the container,
             // immediately terminate
---
+++ profile/ZealandProf.cpp
@@ -0,0 +1,31 @@
+#include "Zealand.hpp"
+#include "VolumeFOV.hpp"
+#include "SphereView.hpp"
+#include "ConeView.hpp"
+
+int main(void)
+{
+    using namespace libzealand;
+    Zealand zealand(17000.0, 17000.0, 17000.0);
+    Vector3 center({0.0,0.0,0.0});
+    double R = 6378.0 + 2000.0;
+    double r = 6378.0 + 400.0;
+    double sma = 7000.0;
+    Vector3 position({0.0,0.0,sma});
+    double range = 5000;
+
+    VolumeFOV* sphere_big = new SphereView(center,R);
+    VolumeFOV* sphere_small = new SphereView(center,r);
+    VolumeFOV* sat = new SphereView(position,range);
+
+    std::vector<VolumeFOV*> shapes({sat,sphere_big});
+    std::vector<VolumeFOV*> not_shapes({sphere_small});
+
+    int level = 4;
+    // for (int i = 0; i < 864; i++)
+    // {
+        zealand.refine(shapes,not_shapes,level);
+    // }
+
+    return 0;
+}
